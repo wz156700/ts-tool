ab3418fb699a9df0109c0e3860a6006685fc53b5
wanglf <3066966420@qq.com>
2023-09-11 13:57:42 +0800

    提交16

--- test.ts
+++ test.ts
@@ -287,3 +287,12 @@ type Result = MyAwaited<ExampleType>; // string
 type MyExclude<T, U> = T extends U ? never : T;
 
 type Result2 = MyExclude<"a" | "b" | "c", "a">; // 'b' | 'c'
+
+// 16
+/**
+ * 实现一个 IF 类型，它接收一个条件类型 C ，一个判断为真时的返回类型 T ，以及一个判断为假时的返回类型 F。 C 只能是 true 或者 false， T 和 F 可以是任意类型。
+ */
+
+type If<C extends Boolean, T, F> = C extends true ? T : F;
+type A = If<true, "a", "b">; // expected to be 'a'
+type B = If<false, "a", "b">; // expected to be 'b'
